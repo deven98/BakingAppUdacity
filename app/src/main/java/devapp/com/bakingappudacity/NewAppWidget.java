3cfa43b1f1f487235dfd1c8ffd94a9a38f4c7440
Deven <deven9852@gmail.com>
2017-11-18 13:16:12 +0530

    Widget list finally displays.

--- app/src/main/java/devapp/com/bakingappudacity/NewAppWidget.java
+++ app/src/main/java/devapp/com/bakingappudacity/NewAppWidget.java
@@ -2,8 +2,12 @@ package devapp.com.bakingappudacity;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import devapp.com.bakingappudacity.utils.WidgetAdapterService;
 
 /**
  * Implementation of App Widget functionality.
@@ -16,23 +20,53 @@ public class NewAppWidget extends AppWidgetProvider {
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        Intent intent = new Intent(context, WidgetAdapterService.class);
+
+        views.setRemoteAdapter(R.id.widget_list_view,intent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+
+                // Create an Intent to launch ExampleActivity
+                //Intent intent = new Intent(context, MainActivity.class);
+                //PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+                // Get the layout for the App Widget and attach an on-click listener
+                // to the button
+                //RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+                //Intent intent = new Intent(context, WidgetAdapterService.class);
+                //views.setRemoteAdapter(R.id.widget_list_view,intent);
+
+                //Intent intent = new Intent(context, WidgetAdapterService.class);
+
+                //views.setRemoteAdapter(R.id.widget_list_view,intent);
+
+                updateAppWidget(context, appWidgetManager, appWidgetId);
+                // Tell the AppWidgetManager to perform an update on the current app widget
+                //appWidgetManager.updateAppWidget(appWidgetId, views);
+
         }
+
+        super.onUpdate(context,appWidgetManager,appWidgetIds);
     }
 
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+
+        //AppWidgetManager appManager = AppWidgetManager.getInstance(context);
+        //ComponentName thisWidget = new ComponentName(context, NewAppWidget.class);
+
+        //updateAppWidget(context,appManager,appManager.getAppWidgetIds(thisWidget));
+
     }
 
     @Override
---
+++ app/src/main/java/devapp/com/bakingappudacity/utils/WidgetAdapterService.java
@@ -0,0 +1,77 @@
+package devapp.com.bakingappudacity.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+import android.widget.Toast;
+
+import devapp.com.bakingappudacity.R;
+
+public class WidgetAdapterService extends RemoteViewsService{
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new WidgetAdapter(this.getApplicationContext());
+    }
+}
+
+class WidgetAdapter implements RemoteViewsService.RemoteViewsFactory {
+
+    private Context context;
+
+    WidgetAdapter(Context mContext){
+
+        context = mContext;
+
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDataSetChanged() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return NetworkUtils.RECIPE_NAMES.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+
+        RemoteViews item = new RemoteViews(context.getPackageName(), R.layout.widget_list_item);
+        item.setTextViewText(R.id.widget_list_view_item_text_view,NetworkUtils.RECIPE_NAMES.get(position));
+
+        return item;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return new RemoteViews(context.getPackageName(),R.layout.widget_list_item);
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+}
